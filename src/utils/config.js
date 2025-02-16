69328f343d6fdcab43d3790b26b7e17571f84235
subodh932 <subodhshrivastava932@gmail.com>
2025-02-16 14:43:32 +0530

    Change API URL

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,5 @@
 export default {
-  api_url: "http://localhost:8080/api/v1",
+    api_url: "https://employee-management-system-backe-production.up.railway.app"
+  //api_url: "http://localhost:8080/api/v1",
+ // api_url: "https://cdac-employee-management-system.vercel.app/"
 };
